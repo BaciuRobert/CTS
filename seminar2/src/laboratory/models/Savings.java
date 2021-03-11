6414e7ed906c3d8d6c3a80510900dfdf41f8604d
BaciuRobert <baciusevastian2008@gmail.com>
2021-03-11 14:36:49 +0200

    before break

--- seminar2/src/laboratory/models/Savings.java
+++ seminar2/src/laboratory/models/Savings.java
@@ -1,5 +1,50 @@
 package laboratory.models;
 
-public class Savings {
+import laboratory.exceptions.IllegalTransferException;
+import laboratory.exceptions.InsufficientFundsException;
+import laboratory.interfaces.Profitable;
+
+public class Savings extends BankAccount implements Profitable{
+	
+	public static final double MIN_BALANCE=100;
+
+	public Savings(String iban, double balance) {
+		super(iban, MIN_BALANCE);
+	}
+
+	@Override
+	public void deposit(Double amount) {
+		this.balance += amount;
+		
+	}
+
+	@Override
+	public void withdraw(Double amount)throws InsufficientFundsException {
+		if(amount >this.balance) {
+			throw new InsufficientFundsException("You need more money");
+		}else {
+			this.balance -= amount;
+		}
+		
+	}
+
+	@Override
+	public void transfer(Account destination, Double amount)throws InsufficientFundsException, IllegalTransferException {
+		if(this == destination) {
+			throw new IllegalTransferException();
+		}
+		this.withdraw(amount);
+		destination.deposit(amount);
+		
+	}
+
+	@Override
+	public void addInterest(double interestPercentRate) {
+		this.balance *=(1+interestPercentRate/100);
+		
+	}
+	
+	
+	
 
 }
