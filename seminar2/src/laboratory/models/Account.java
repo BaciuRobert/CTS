293a81ad56f984d6cc2b33918c98ec4406398247
BaciuRobert <baciusevastian2008@gmail.com>
2021-03-11 14:37:10 +0200

    before the break

--- seminar2/src/laboratory/models/Account.java
+++ seminar2/src/laboratory/models/Account.java
@@ -1,13 +1,16 @@
 package laboratory.models;
 
+import laboratory.exceptions.IllegalTransferException;
+import laboratory.exceptions.InsufficientFundsException;
+
 public abstract class Account {
 	public abstract double getBalance();
 	
 	public abstract void deposit(Double amount);
 	
-	public abstract void withdraw(Double amount);
+	public abstract void withdraw(Double amount)throws InsufficientFundsException, InsufficientFundsException;
 	
-	public abstract void transfer(Account destination,Double amount);
+	public abstract void transfer(Account destination,Double amount) throws InsufficientFundsException, IllegalTransferException;
 	
 	
 
--- seminar2/src/laboratory/models/BankAccount.java
+++ seminar2/src/laboratory/models/BankAccount.java
@@ -1,5 +1,22 @@
 package laboratory.models;
 
-public abstract class BankAccount {
+public abstract class BankAccount extends Account {
+   protected String iban;
+   protected double balance;
 
+   public BankAccount(String iban,double balance) {
+	   this.balance=balance;
+	   this.iban=iban;
+   }
+   
+   
+   
+   @Override
+   public double getBalance() {
+	return this.balance;
+}
+   
+   
+  
+   
 }
