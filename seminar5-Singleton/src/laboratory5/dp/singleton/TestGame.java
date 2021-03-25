83ca6b5e5c5608861e20f84e6548e7e8e8801a92
BaciuRobert <baciusevastian2008@gmail.com>
2021-03-25 14:32:53 +0200

    singleton standard-before break

---
+++ seminar5-Singleton/src/laboratory5/dp/singleton/TestGame.java
@@ -0,0 +1,18 @@
+package laboratory5.dp.singleton;
+
+public class TestGame {
+
+	public static void main(String[] args) {
+		
+		//RESTBackend backend=new RESTBackend("acs.ase.ro/cts","get some data");
+		RESTBackend backend=RESTBackend.getRESTBackend();
+		
+		
+		UIModule uiModule=new UIModule();
+		PlayerModule playerModule=new PlayerModule();
+		
+	
+
+	}
+
+}
---
+++ seminar5-Singleton/src/laboratory5/dp/singleton/UIModule.java
@@ -0,0 +1,9 @@
+package laboratory5.dp.singleton;
+
+public class UIModule {
+
+    public UIModule() {
+    	//RESTBackend backend=new RESTBackend("acs.ase.ro/cts","get some data");
+    	RESTBackend backend=RESTBackend.getRESTBackend();
+    }
+}
