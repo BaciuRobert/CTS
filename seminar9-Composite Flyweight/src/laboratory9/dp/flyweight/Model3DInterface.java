3daefb6576680f88f1ce76bdfcb2eca43402d922
BaciuRobert <baciusevastian2008@gmail.com>
2021-04-22 15:21:04 +0300

    final commit-flyweight

---
+++ seminar9-Composite Flyweight/src/laboratory9/dp/flyweight/Model3DInterface.java
@@ -0,0 +1,8 @@
+package laboratory9.dp.flyweight;
+
+public interface Model3DInterface {
+
+	public void display(ScreenData screenDetails);
+	
+	
+}
---
+++ seminar9-Composite Flyweight/src/laboratory9/dp/flyweight/ModelType.java
@@ -0,0 +1,5 @@
+package laboratory9.dp.flyweight;
+
+public enum ModelType {
+BUILDING,SOLDIER
+}
